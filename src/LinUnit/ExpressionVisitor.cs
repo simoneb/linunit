51118a13d755e7aecfa2f540224a049aac118adc
Simone Busoli <simone.busoli@gmail.com>
2009-03-31 23:42:42 +0200

    Moved constraint to its own file.

--- src/LinUnit/ExpressionVisitor.cs
+++ src/LinUnit/ExpressionVisitor.cs
@@ -147,27 +147,4 @@ namespace LinUnit
             builder.Append(new NotOperator());
         }
     }
-
-    internal class BooleanMethodCallExpressionConstraint<TActual> : Constraint
-    {
-        private readonly MethodCallExpression expression;
-        private readonly ParameterExpression parameter;
-
-        public BooleanMethodCallExpressionConstraint(MethodCallExpression expression, ParameterExpression parameter)
-        {
-            this.expression = expression;
-            this.parameter = parameter;
-        }
-
-        public override bool Matches(object act)
-        {
-            actual = act;
-            return Expression.Lambda<Func<TActual, bool>>(expression, parameter).Compile().Invoke((TActual) actual);
-        }
-
-        public override void WriteDescriptionTo(MessageWriter writer)
-        {
-            writer.WriteExpectedValue(expression.ToString());
-        }
-    }
 }
